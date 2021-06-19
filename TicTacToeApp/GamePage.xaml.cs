f6009bc65592ffc69e0f6f47a3cddbc3fdd8ae94
stu2001737006 <stu2001737006@uni-plovdiv.bg>
2021-06-19 08:36:59 +0300

    Initial commit

---
+++ TicTacToeApp/GamePage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TicTacToeApp
+{
+    public partial class GamePage : ContentPage
+    {
+        public GamePage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+
+        /*void checkwinner(string symbol) 
+        {
+            if (button1.Text == symbol || button2.Text == symbol || button3.Text == symbol)
+            {
+                MessageBox.Show(symbol+"Won the game");
+            }
+        } */
+        void Fnsymbol(object senderobj) 
+        {
+            string buttontxt = ((Button)senderobj).Text;
+            if (buttontxt == "")
+            {
+                if (count % 2 == 0)
+                {
+                    ((Button)senderobj).Text = "O";
+                }
+                else
+                {
+                    ((Button)senderobj).Text = "X";
+                }
+                count++;
+            }
+          
+        }
+
+        private void Button1_clicked(object sender, EventArgs e) 
+        {
+            Fnsymbol(sender);
+        }
+        private void Button2_clicked(object sender, EventArgs e)
+        {
+            Fnsymbol(sender);
+        }
+        private void Button3_clicked(object sender, EventArgs e)
+        {
+            Fnsymbol(sender);
+        }
+        private void Button4_clicked(object sender, EventArgs e)
+        {
+            Fnsymbol(sender);
+        }
+        private void Button5_clicked(object sender, EventArgs e)
+        {
+            Fnsymbol(sender);
+        }
+        private void Button6_clicked(object sender, EventArgs e)
+        {
+            Fnsymbol(sender);
+        }
+        private void Button7_clicked(object sender, EventArgs e)
+        {
+            Fnsymbol(sender);
+        }
+        private void Button8_clicked(object sender, EventArgs e)
+        {
+            Fnsymbol(sender);
+        }
+        private void Button9_clicked(object sender, EventArgs e)
+        {
+            Fnsymbol(sender);
+        }
+        private void BtnNewGame_Click(object sender, EventArgs e) 
+        {
+            
+        }
+
+        private void ContentView_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TicTacToeApp/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace TicTacToeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string name1 = Player1Entry.Text;
+            Player1Label.Text = "Играч 1 (X): " + name1;
+            Player1Entry.Text = "";
+       
+            string name2 = Player2Entry.Text;
+            Player2Label.Text = "Играч 2 (O): " + name2;
+            Player2Entry.Text = "";
+        
+        }
+        private void Button2_Clicked(object sender, EventArgs e)
+        {
+
+            Application.Current.MainPage = new NavigationPage(new GamePage());
+
+
+        }
+
+    }
+}
